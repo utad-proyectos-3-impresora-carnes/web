56a4668b58702e49db4b17f7ba84d13c7d738d49
acalde02 <69548230+acalde02@users.noreply.github.com>
2025-02-05 19:32:53 +0100

    cambios

--- web-gestion-carnes/src/app/page.js
+++ web-gestion-carnes/src/app/page.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 async function sendQuery(contents) {
 
 	const link = `${process.env.NEXT_PUBLIC_API_URL}${contents}`;
-
+w
 	console.log("Calling", link);
 
 	return fetch(link);
@@ -29,7 +29,7 @@ export default function Home() {
 
 	return (
 		<div>
-			<h1>Da printer app!</h1>
+			<h1>Da Adrian app!</h1>
 			<p>The server says {serverAnswer}</p>
 			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("")}>/</button>
 			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("home")}>/home</button>
