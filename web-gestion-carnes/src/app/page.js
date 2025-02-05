71c8f6b66c70088fe16c0805e504e4152dfc5473
acalde02 <69548230+acalde02@users.noreply.github.com>
2025-02-05 19:34:51 +0100

    prueba

--- web-gestion-carnes/src/app/page.js
+++ web-gestion-carnes/src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
 
 	return (
 		<div>
-			<h1>Da Adrian app!</h1>
+			<h1>Da printer app!</h1>
 			<p>The server says {serverAnswer}</p>
 			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("")}>/</button>
 			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("home")}>/home</button>
