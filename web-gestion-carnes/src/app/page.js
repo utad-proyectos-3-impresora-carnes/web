82c351406bf69df6801427f57f526bd9e98abf5c
acalde02 <69548230+acalde02@users.noreply.github.com>
2025-02-05 19:35:50 +0100

    Revert "a"
    This reverts commit 7ac3dcf5e2f7f157746e3b1d34e1e49c63cebe15.

---
+++ web-gestion-carnes/src/app/page.js
@@ -0,0 +1,40 @@
+"use client"
+import { useState } from "react";
+
+// Ejemplo de como usar el servidor
+async function sendQuery(contents) {
+
+	const link = `${process.env.NEXT_PUBLIC_API_URL}${contents}`;
+w
+	console.log("Calling", link);
+
+	return fetch(link);
+
+}
+
+export default function Home() {
+
+	const [serverAnswer, setServerMessage] = useState("");
+
+	// Ejemplo de como usar el servidor
+	const askServer = (parameter) => {
+		console.log("Sending param", parameter);
+		sendQuery(parameter).then((result) => {
+			const text = result.text();
+			text.then((plainText) => {
+				setServerMessage(plainText);
+			})
+		})
+	}
+
+	return (
+		<div>
+			<h1>Da printer app!</h1>
+			<p>The server says {serverAnswer}</p>
+			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("")}>/</button>
+			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("home")}>/home</button>
+			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("test")}>/test</button>
+			<button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5" onClick={() => askServer("user")}>/user</button>
+		</div>
+	);
+}
