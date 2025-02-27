46de68c7faa18c0a9533fbb6d48a86697b523509
acalde02 <69548230+acalde02@users.noreply.github.com>
2025-02-27 13:14:55 +0100

    cambios

--- web-gestion-carnes/src/app/dashboard/page.js
+++ web-gestion-carnes/src/app/dashboard/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import FilterSidebar from "../components/filters";
-import Table from "../components/table";
+import FilterSidebar from "../Components/filters";
+import Table from "../Components/table";
 
 export default function Page() {
   const [data, setData] = useState([]);
