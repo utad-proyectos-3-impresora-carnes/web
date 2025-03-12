18025a52d875214fe658fad70bc15da51df715c9
acalde02 <a_calderon_007@hotmail.com>
2025-03-13 00:14:16 +0100

    implementado api de login y de registro

--- web-gestion-carnes/src/app/dashboard/page.js
+++ web-gestion-carnes/src/app/dashboard/page.js
@@ -11,25 +11,54 @@ export default function Page() {
   const[error, setError] = useState(null);
 
   useEffect(() => {
-//    fetch("https://api-hxge.onrender.com/api/member")
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/member`)
-      .then((response) => 
-      {
-        if (!response.ok) {
-          throw new Error("Error ${response.status}: No se pudieron cargar los miembros");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        setLoading(false);
+    const fetchMembersData = async () => {
+      const token = localStorage.getItem('token'); // Obtiene el token almacenado
+  
+      if (!token) {
+        console.error("Error: No token found. Redirecting to login...");
         setError(true);
-      });
+        setMessage('Error: No token found. Please login first.');
+        setLoading(false);
+        router.push('/'); // Redirige al login
+        return;
+      }
+  
+      try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/member`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        });
+  
+        if (response.status === 401) {
+          throw new Error("Token expirado. Redirigiendo al login...");
+        }
+  
+        if (!response.ok) {
+          throw new Error(`Error ${response.status}: No se pudieron cargar los miembros`);
+        }
+  
+        const data = await response.json();
+        setData(data);
+      } catch (error) {
+        console.error("Error:", error);
+        setError(true);
+        setMessage(error.message || "Error al cargar los miembros");
+  
+        if (error.message.includes("Token expirado")) {
+          localStorage.removeItem('token'); // Elimina el token caducado
+          router.push('/'); // Redirige al login
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+  
+    fetchMembersData();
   }, []);
+  
 
   const handleApplyFilters = (filters) => {
     setFilters(filters);
--- web-gestion-carnes/src/app/page.js
+++ web-gestion-carnes/src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 
@@ -10,6 +10,7 @@ export default function Home() {
   const [error, setError] = useState(null);
   const router = useRouter();
   const [rotation, setRotation] = useState({ x: 0, y: 0 });
+
 
   const handleMouseMove = (e) => {
     const { clientX, clientY, currentTarget } = e;
@@ -24,23 +25,30 @@ export default function Home() {
   };
 
   const handleLogin = async (e) => {
-    e.preventDefault();
-    setError(null);
+      e.preventDefault();
+      setError(null);
 
-    const response = await fetch('/Dummies/user_credentials.json');
-    if (!response.ok) {
-      setError('Error loading credentials');
-      return;
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/login`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ email, password }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.message || 'Ha ocurrido un error.');
+        }
+
+        localStorage.setItem('token', data.token);
+        router.push('/dashboard');
+      }
+      catch (error) {
+        setError(error.message || 'Ha ocurrido un error.');
+
     }
 
-    const data = await response.json();
-    const user = data.users.find(u => u.username === email && u.password === password);
-
-    if (user) {
-      router.push('/dashboard'); // Redirigir a la página principal después del login
-    } else {
-      setError('Credenciales inválidas');
-    }
   };
 
   return (
