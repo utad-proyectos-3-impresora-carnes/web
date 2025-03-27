004732454787dbf8b0f29878713996e478918bc6
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-03-27 10:03:00 +0100

    Handle call server error

--- web-gestion-carnes/src/services/callServer.ts
+++ web-gestion-carnes/src/services/callServer.ts
@@ -1,12 +1,20 @@
 "use server"
 
-export default async function callServer(url: string, token: string, options: RequestInit): Promise<any> {
+export default async function callServer(url: string, token: string | null, options: RequestInit): Promise<any> {
 	try {
 		options.headers = {
 			"Content-Type": "application/json",
-			Authorization: `Bearer ${token}`,
+			Authorization: `Bearer ${token}`
 		}
-		return await (await fetch(process.env.NEXT_PUBLIC_API_URL + url, options)).json();
+
+		const res = await fetch(process.env.NEXT_PUBLIC_API_URL + url, options);
+
+		if (res.status === 200 || res.status === 201) {
+			return await res.json();
+		} else {
+			console.log(res)
+			throw new Error("La request a [" + url + "] fallo!");
+		}
 	} catch (error) {
 		console.error(error);
 		throw (error);
