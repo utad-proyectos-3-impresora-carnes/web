d6edb8041d50bd14e6654c787348a86c8bed1612
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-04-26 11:57:16 +0200

    Redone token handling

--- web-gestion-carnes/src/services/tokenHandler.js
+++ web-gestion-carnes/src/services/tokenHandler.js
@@ -1,4 +1,23 @@
-"use client"
-export function fetchToken() {
-	return localStorage.getItem("token");
+"use server"
+
+import { cookies } from 'next/headers'
+
+
+/**
+ * Obtiene el token del usuario.
+ * @returns El token del usuario.
+ */
+export async function fetchToken() {
+	const cookieStore = await cookies();
+	const userToken = cookieStore.get('userToken')?.value;
+	return userToken;
+}
+
+/**
+ * Sets the user token.
+ * @param {string} token The user token
+ */
+export async function setToken(token) {
+	const cookieStore = await cookies();
+	cookieStore.set("userToken", token);
 }
