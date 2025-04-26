07c48b8fc8417a5a57afd830efd80ca9316de28c
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-04-26 12:01:20 +0200

    Changed callServer

--- web-gestion-carnes/src/services/callServer.js
+++ web-gestion-carnes/src/services/callServer.js
@@ -1,22 +1,38 @@
 "use server"
 
+/**
+ * Generates a query to the server,
+ * @param {string} url The url of the query.
+ * @param {string} token The token for autenticathion.
+ * @param {Object} options The options of the query.
+ * @returns The query result
+ */
 export default async function callServer(url, token, options = {}) {
+
 	try {
+
+		// Adds headers
 		options.headers = {
 			"Content-Type": "application/json",
 			Authorization: `Bearer ${token}`
 		}
 
+		// Makes the query.
 		const res = await fetch(process.env.NEXT_PUBLIC_API_URL + url, options);
 
-		if (res.status === 200 || res.status === 201) {
-			return await res.json();
-		} else {
-			console.log(res)
+		// Si la query falla, lanza error.
+		if (res.ok !== true)
 			throw new Error("La request a [" + url + "] fallo!");
-		}
+
+		// Return query result.
+		return await res.json();
+
 	} catch (error) {
+
 		console.error(error);
+
+		// Throw error upstream to handle case by case.
 		throw (error);
+
 	}
 }
