e07d69935a5a4bbf396ad79ffc03fcef6e819bf2
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-05-07 19:07:50 +0200

    Formatted stuff

--- web-gestion-carnes/src/components/loading.jsx
+++ web-gestion-carnes/src/components/loading.jsx
@@ -1,12 +1,12 @@
-'use client'; // Correcto para mostrar UI interactiva
+"use client"; // Correcto para mostrar UI interactiva
 
 import { CircularProgress } from '@mui/material';
 
 export default function Loading() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
-      <CircularProgress size={40} thickness={4.5} />
-      <p className="mt-4 text-gray-700 text-sm">Cargando la página...</p>
-    </div>
-  );
+	return (
+		<div className="min-h-screen flex flex-col items-center justify-center bg-white">
+			<CircularProgress size={40} thickness={4.5} />
+			<p className="mt-4 text-gray-700 text-sm">Cargando la página...</p>
+		</div>
+	);
 }
