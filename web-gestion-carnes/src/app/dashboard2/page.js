af46fd4e9f97965dd162ef0937fafc48c318618d
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-05-10 19:13:22 +0200

    Started new dashboard

---
+++ web-gestion-carnes/src/app/dashboard2/page.js
@@ -0,0 +1,50 @@
+"use client"
+
+import { useState, useEffect } from "react";
+
+import Header from "@/components/header";
+import FilterSidebar from "@/components/FilterSiderbar";
+
+/**
+ * The dashboard page.
+ * @returns Dahsboard page.
+ */
+export default async function Page() {
+
+	const [filters, setFilters] = useState({
+		fullName: null,
+		dni: null,
+		group: null,
+		year: null,
+		printed: null,
+		validationState: null
+	});
+
+	const changeFilters = (category, value) => {
+		setFilters(previousFilters => ({
+			...previousFilters,
+			[category]: value
+		}));
+	}
+
+	useEffect(() => {
+
+		console.log(filters);
+
+	}, [filters])
+
+
+
+	return (
+		<div className="h-screen w-full overflow-hidden">
+			<Header />
+			<p>Dashboard two!</p>
+			<div className="flex pt-16 h-[calc(100vh-4rem)] overflow-hidden">
+				{/* Sidebar */}
+				<aside className="w-64 h-full shrink-0 mt-8">
+					<FilterSidebar changeFilters={changeFilters} />
+				</aside>
+			</div>
+		</div>
+	);
+}
