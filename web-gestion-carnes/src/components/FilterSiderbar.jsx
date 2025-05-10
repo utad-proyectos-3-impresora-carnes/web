7d645a4c9a4f037e1569d2cf8cf36b469537c76c
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-05-10 19:57:22 +0200

    Redone group filters

--- web-gestion-carnes/src/components/FilterSiderbar.jsx
+++ web-gestion-carnes/src/components/FilterSiderbar.jsx
@@ -3,6 +3,7 @@
 import { ChevronDown } from "@deemlol/next-icons";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { getAllGroups } from "@/services/group";
 
 /**
  * El componente con los filtros de los carnes.
@@ -10,10 +11,15 @@ import { useEffect, useState } from "react";
  * @returns Componente con los filtros.
  */
 export default function FilterSidebar({ changeFilters }) {
+
+	const changeGroupValue = (groupId) => {
+		changeFilters("group", groupId);
+	}
+
 	return (
 		<div className="w-64 text-white px-0 py-5 fixed top-16 left-0 bottom-0 overflow-y-auto ml-[-4px] mt-[-15px]">
 			<div className="flex flex-col">
-				<CommonFilterPannel filterTitle={"Titulación"} filterContents={<GroupFilter />} />
+				<CommonFilterPannel filterTitle={"Titulación"} filterContents={<GroupFilter changeGroupValue={changeGroupValue} />} />
 				<CommonFilterPannel filterTitle={"Estado"} filterContents={<ValidationStateFilter />} />
 				<CommonFilterPannel filterTitle={"Año Académico"} filterContents={<AcademicYearFilter />} />
 				<CommonFilterPannel filterTitle={"Impreso"} filterContents={<PrintedStateFilter />} />
@@ -56,47 +62,81 @@ function CommonFilterPannel({ filterTitle, filterContents }) {
 	);
 }
 
-
-function CommonFilterOption() {
+/**
+ * Common option picker for the filters.
+ * @returns Component to pick options in the filters
+ */
+function CommonFilterOption({ id, name, value, selected, onChange }) {
 	const labelStyle = "flex items-center space-x-3 py-2 pl-5 text-[15px] text-white";
 	const radioStyle = "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500";
-	return {/* {loading ? (
-							<p className="text-gray-400 pl-5 text-sm">Cargando...</p>
-						) : (
-							titulaciones.map((t) => (
-								<label key={t._id} className={labelStyle}>
-									<input
-										type="radio"
-										className={radioStyle}
-										name="titulacion"
-										checked={selected.group === t.name}
-										onChange={() => handleChange("group", t.name)}
-									/>
-									<span>{t.name}</span>
-								</label>
-							))
-						)} */}
+	return (
+
+		<label key={id} className={labelStyle}>
+			<input
+				type="radio"
+				className={radioStyle}
+				checked={selected === value}
+				onChange={onChange}
+			/>
+			<span>{name}</span>
+		</label>
+
+	);
 }
 
-function GroupFilter() {
+function GroupFilter({ changeGroupValue }) {
+
+	const [groups, setGroups] = useState([]);
+	const [loading, setLoading] = useState(true)
+	const [selectedGroup, setSelectedGroup] = useState(null);
+
+	const updateGroups = (newGroups) => {
+		setGroups(newGroups);
+	}
+
+	useEffect(() => {
+		getAllGroups()
+			.then(groups => {
+				setLoading(false)
+				updateGroups(groups)
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
+
+	useEffect(() => {
+		console.log(selectedGroup)
+		changeGroupValue(selectedGroup);
+	}, [selectedGroup])
+
+	return (
+		<>
+			{
+				loading ?
+					(<p className="text-gray-400 pl-5 text-sm" > Cargando...</p>)
+					: groups.map(group => {
+						return (
+							<CommonFilterOption id={group._id} name={group.name} value={group._id} selected={selectedGroup} onChange={() => setSelectedGroup(group._id)} />
+						)
+					})
+			}
+		</>);
+}
+
+function ValidationStateFilter({ changeValidationStateValue }) {
 	return (
 		<div></div>
 	);
 }
 
-function ValidationStateFilter() {
+function AcademicYearFilter({ changeAcademicYearValue }) {
 	return (
 		<div></div>
 	);
 }
 
-function AcademicYearFilter() {
-	return (
-		<div></div>
-	);
-}
-
-function PrintedStateFilter() {
+function PrintedStateFilter({ changePrintedStateValue }) {
 	return (
 		<div></div>
 	);
