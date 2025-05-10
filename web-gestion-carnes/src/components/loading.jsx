c85e1bf354fe2a09f19c325159488d1230cf0ed2
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-05-10 18:38:41 +0200

    Finished loading component

--- web-gestion-carnes/src/components/loading.jsx
+++ web-gestion-carnes/src/components/loading.jsx
@@ -1,16 +1,16 @@
 "use client"; // Correcto para mostrar UI interactiva
 
-import { CircularProgress } from '@mui/material';
+import { CircularProgress } from '@mui/material/CircularProgress';
 
 /**
  * This is a placeholder component to show an action is loading (usually when a query is sent to server).
  * @returns The loading component.
  */
-export default function Loading() {
+export default function Loading({ loadingText = "Cargando la página..." }) {
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center bg-white">
 			<CircularProgress size={40} thickness={4.5} />
-			<p className="mt-4 text-gray-700 text-sm">Cargando la página...</p>
+			<p className="mt-4 text-gray-700 text-sm">{loadingText}</p>
 		</div>
 	);
 }
